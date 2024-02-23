2f35b8b5cdb9104170a6a2ff40e02badd61f2930
Anders Krarup <andkrup@gmail.com>
2024-02-23 10:26:00 +0100

    copy to clipboard

--- Edge Plugin/popup.js
+++ Edge Plugin/popup.js
@@ -1,15 +1,49 @@
-const sendMessageId = document.getElementById("btn_domain");
-if (sendMessageId) {
-  sendMessageId.onclick = function(e) {
+const svrNameButton = document.getElementById("btn_servername");
+const domainButton = document.getElementById("btn_domain");
 
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) 
-    {
+if (domainButton) {
+    domainButton.onclick = function(e) {
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
             var tab = tabs[0];
-
             var a = document.createElement('a');
             a.href = tab.url;
-            alert(a.hostname);
-            
-    });
-  };
+            if(a.hostname.indexOf('.') > 0){
+                var frags = a.hostname.split('.').reverse();
+                var hostnameFrags = [];
+                for(var i = 0; i < 2; i++){
+                    hostnameFrags.unshift(frags[i]);
+                }
+                var hostname = hostnameFrags.join('.');
+                var blob = new Blob([hostname], {type: 'text/plain'});
+                var item = new ClipboardItem({'text/plain': blob});
+                
+                navigator.clipboard.write([item]).then(function() {
+                    console.log("Copied to clipboard successfully!");
+                }, function(error) {
+                    console.error("unable to write to clipboard. Error:");
+                    console.log(error);
+                });
+            }
+        });
+    };
+}
+
+if (svrNameButton) {
+    svrNameButton.onclick = function(e) {
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+            var tab = tabs[0];
+            var a = document.createElement('a');
+            a.href = tab.url;
+            var hostname = a.hostname;
+            var blob = new Blob([hostname], {type: 'text/plain'});
+            var item = new ClipboardItem({'text/plain': blob});
+
+            navigator.clipboard.write([item]).then(function() {
+                console.log("Copied to clipboard successfully!");
+            }, function(error) {
+                console.error("unable to write to clipboard. Error:");
+                console.log(error);
+            });
+        });
+    };
 }
