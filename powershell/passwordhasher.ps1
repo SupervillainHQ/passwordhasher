7f3e2fe7ca1613076077e2b3a0eb01aa026fe43f
Anders Krarup <andkrup@gmail.com>
2025-07-25 13:10:40 +0200

    refactored powershell script
    changed special-char and capitalisation functions + parameters

--- powershell/passwordhasher.ps1
+++ powershell/passwordhasher.ps1
@@ -1,47 +1,41 @@
 param(
-    [switch]$firstCap = $false,
-    [switch]$firstNum = $false
+    [Alias("c")][ValidateRange(0, 16)][int]$Cap = 0,
+    [Alias("s")][ValidateRange(0, 16)][int]$Specials = 0,
+    [ValidateRange(6, 32)][int]$Len = 32,
+    [Alias("h")][switch]$Help = $false
 )
 
 Add-Type -AssemblyName 'System.Windows.Forms'
 
-function FirstCap{
+function ChangeToUpper{
     param(
-        [string]$hash
+        [string]$hexString
     )
-
-    $stack = ""
-    $first = $false
-    foreach ($char in [char[]]$hash) {
-        if([int]($char) -gt 96 -and [int]($char) -lt 103 -and $first -ne $true){
-            $char = [char]($char - 32)
-            $first = $true
-        }
-        $stack = $stack + $char
-    }
-
-    return $stack
+    $dec = [Convert]::ToString([uint32]"0x000000$hexString", 10)
+    $modded = ($dec % 24) + 65
+    $char = [char[]]$modded
+    return $char
 }
 
-function FirstNum{
+function ChangeToSpecial{
     param(
-        [string]$hash
+        [string]$hexString
     )
+    $dec = [Convert]::ToString([uint32]"0x000000$hexString", 10)
+    $modded = ($dec % 7) + 58
+    $char = [char[]]$modded
+    return $char
+}
 
-    $subs = "$!@#?%&/()"
-    $stack = ""
-    $first = $false
-    foreach ($char in [char[]]$hash) {
-        if([int]($char) -gt 47 -and [int]($char) -lt 58 -and $first -ne $true){
-            $index = [int]($char) - 48
-            $char = $subs.SubString($index, 1)
-            $first = $true
-        }
-        $stack = $stack + $char
-    }
-
-    Write-Host $stack
-    return $stack
+if($Help){
+    Write-Host "Passwordhasher (Powershell) 2.0"
+    Write-Host "  Creates a 32 character hash from the clipboard contents and a secret."
+    Write-Host "  Use the hash as password by copying a website domain to the clipboard"
+    Write-Host "  and input a secret, then submit the clipboard contents as the pass-"
+    Write-Host "  word. This way you can reuse the secret across several website domains"
+    Write-Host "Usage"
+    Write-Host "  Passwordhasher.ps1 [-Cap|c <int>][-Specials|s <int>]"
+    exit 0
 }
 
 $utf8 = new-object -TypeName System.Text.UTF8Encoding
@@ -56,13 +50,22 @@ $stream = [System.IO.MemoryStream]::new($utf8.GetBytes($clipped))
 $hash = Get-FileHash -Algorithm MD5 -InputStream $stream | Select-Object -ExpandProperty Hash
 $hash = $hash.ToLower()
 
-Write-Host $hash
-if($firstCap){
-    $hash = FirstCap $hash.ToLower()
+if($Cap -gt 0){
+    $capped = ""
+    for($i = 1; $i -le $Cap; $i++){
+        $sub = $i * 2
+        $capped = $capped + (ChangeToUpper $hash.SubString(32 - $sub, 2))
+    }
 }
-if($firstNum){
-    $hash = FirstNum $hash
+if($Specials -gt 0){
+    $special = ""
+    for($i = 1; $i -le $Specials; $i++){
+        $sub = $i * 2
+        $special = $special + (ChangeToSpecial $hash.SubString(($i-1) * 2, 2))
+    }
 }
 
-[System.Windows.Forms.Clipboard]::SetText($hash)
+$hash = $hash.SubString($Specials, $Len - $Specials - $Cap)
+
+[System.Windows.Forms.Clipboard]::SetText("$special$hash$capped")
 
