e75fdee06402cf95b689b888673747196b2d8692
Anders <anders@andkrup.dk>
2026-02-10 14:29:36 +0100

    relocate php dev files

--- php/app.php
+++ php/app.php
@@ -2,7 +2,7 @@
 
 use svhq\passwordhasher\Passwordhasher;
 
-require_once './vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $hasher = new Passwordhasher('1');
 $hasher->hashIt()->changeLength(26)->changeToUpper(3)->changeToSpecial(2);
